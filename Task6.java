e2f69a8475fb3f0d2d84037838276d4f2fc6c427
HUSEYIN OZDEMIR <78041148+HsynHsyn@users.noreply.github.com>
2024-05-11 10:08:33 +0100

    Add files via upload

---
+++ Task6.java
@@ -0,0 +1,13 @@
+package Day3_Variable_DataTypes;
+
+public class Task6 {
+	public static void main(String[] args) {
+		int flightNum=89,travelTime=2,departure=10,distance=5000;
+		System.out.println(flightNum);
+		System.out.println(travelTime);
+		System.out.println(departure);
+		System.out.println(distance);
+		System.out.println("flightNum :");
+	}
+
+}
---
+++ test1.java
@@ -0,0 +1,19 @@
+
+package Day3_Variable_DataTypes;
+public class test1 {
+    public static void main(String[] args) {
+       /* 2. Write a Java program to print the sum of two numbers.
+		Test Data:
+		74 + 36
+		Expected Output :
+		110*/
+    	
+    	// answer
+    	/* int sum1,sum2,total;
+    	sum1 = 5;
+    	sum2 = 40;
+    	total = sum1 + sum2;
+    	System.out.println(total);*/
+        
+    }
+}
